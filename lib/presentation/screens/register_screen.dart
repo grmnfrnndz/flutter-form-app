9bb4b911ceb9ca8fab330764c7b819ba13b8a93f
German Contreras <inginf.german.fernandez@gmail.com>
2023-08-23 20:18:09 -0400

    form manager with statefullwidget

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -35,31 +35,73 @@ class RegisterScreen extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String username = '';
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
           CustomTextFormField(
             label: 'Username',
+            onChanged: (value) => username = value,
+            validator: (String? value) {
+              if (value == null || value.isEmpty) return 'Field Required';
+              if (value.trim().isEmpty) return 'Field Required';
+              if (value.length < 6) return 'Username must be greather length 6 characters';
+              return null;
+            },
           ),
           const SizedBox(height: 10,),
           CustomTextFormField(
             label: 'Email',
+            onChanged: (value) => email = value,
+            validator: (String? value) {
+              if (value == null || value.isEmpty) return 'Field Required';
+              if (value.trim().isEmpty) return 'Field Required';
+              final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+              if (!emailRegExp.hasMatch(value)) return 'Email Invalid';
+
+              return null;
+            },
           ),
           const SizedBox(height: 10,),
           CustomTextFormField(
             label: 'Password',
+            onChanged: (value) => password = value,
+            validator: (String? value) {
+              if (value == null || value.isEmpty) return 'Field Required';
+              if (value.trim().isEmpty) return 'Field Required';
+              if (value.length < 6) return 'Username must be greather length 6 characters';
+
+              return null;
+            },
             obscureText: true,
           ),
           
 
           const SizedBox(height: 20,),
 
-          FilledButton.tonalIcon(onPressed: () {}, icon: const Icon(Icons.save), label: const Text('Create User'),),
+          FilledButton.tonalIcon(onPressed: () {
+
+            final isValid = _formKey.currentState!.validate();
+            if (!isValid) return;
+
+            print('$username - $password - $email');
+          }, icon: const Icon(Icons.save), label: const Text('Create User'),),
         ],
       ),
     );
